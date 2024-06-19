53fce9674ab1a2cd3ca5a4ca704ed12eb31f7db7
Nadir Kerem Cetin <nadirkerem12@gmail.com>
2024-06-18 23:48:24 -0400

    complete landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import LandingImage from "@/assets/job_hunt.svg";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <header className="mx-auto max-w-6xl px-4 pt-4 sm:px-8">
+        <h1 className="text-center text-3xl font-bold text-blue-700 sm:text-4xl">
+          JobHunt
+        </h1>
+      </header>
+      <section className="mx-auto -mt-10 grid h-screen max-w-6xl items-center px-10 lg:grid-cols-[1fr,400px]">
+        <div>
+          <h1 className="text-5xl font-bold capitalize">
+            find your <span className="text-primary">dream</span> job
+          </h1>
+          <p className="mt-4 max-w-md leading-loose">
+            JobHunt is a platform that helps you find your dream job. It allows
+            you to search for jobs, apply for them and track the application
+            status.
+          </p>
+          <Button asChild className="mt-4">
+            <Link href="/jobs">Get Started</Link>
+          </Button>
+        </div>
+        <Image src={LandingImage} alt="landing" className="hidden lg:block" />
+      </section>
+    </main>
+  );
 }
