8b7263e5bc0601a72036074bb4bebc3155edba3a
Nadir Kerem Cetin <nadirkerem12@gmail.com>
2024-07-08 10:33:43 -0400

    setup react query and toaster

--- app/(dashboard)/add-job/page.tsx
+++ app/(dashboard)/add-job/page.tsx
@@ -1,9 +1,15 @@
 import CreateJobForm from "@/components/create-job-form";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 
 export default function AddJobPage() {
+  const queryClient = new QueryClient();
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <CreateJobForm />
-    </>
+    </HydrationBoundary>
   );
 }
