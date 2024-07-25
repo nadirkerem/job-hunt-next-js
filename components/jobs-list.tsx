f8110165630f8158abe953287f1104ace486b2e3
Nadir Kerem Cetin <nadirkerem12@gmail.com>
2024-07-24 23:39:08 -0400

    jobs list setup

--- components/jobs-list.tsx
+++ components/jobs-list.tsx
@@ -1,3 +1,38 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import { getAllJobsAction } from "@/utils/actions";
+import { useQuery } from "@tanstack/react-query";
+
+import JobCard from "./job-card";
+
 export default function JobsList() {
-  return <div>JobsList</div>;
+  const searchParams = useSearchParams();
+  const search = searchParams.get("search") || "";
+  const jobStatus = searchParams.get("jobStatus") || "all";
+  const pageNumber = Number(searchParams.get("page")) || 1;
+
+  const { data, isPending } = useQuery({
+    queryKey: ["jobs", search, jobStatus, pageNumber],
+    queryFn: () => getAllJobsAction({ search, jobStatus, page: pageNumber }),
+  });
+
+  const jobs = data?.jobs || [];
+
+  if (isPending) {
+    return <h2 className="text-xl">Please wait...</h2>;
+  }
+
+  if (jobs.length < 1) {
+    return <h2 className="text-xl">No jobs found.</h2>;
+  }
+
+  return (
+    <>
+      <div className="grid gap-8 md:grid-cols-2">
+        {jobs.map((job) => {
+          return <JobCard key={job.id} job={job} />;
+        })}
+      </div>
+    </>
+  );
 }
