76d5167f6267cc914c416bb55e4fbbd77f78b68c
Nadir Kerem Cetin <nadirkerem12@gmail.com>
2024-07-25 00:17:45 -0400

    job card setup

--- components/job-card.tsx
+++ components/job-card.tsx
@@ -1,5 +1,39 @@
+import Link from "next/link";
+import { MapPin, Briefcase, CalendarDays, RadioTower } from "lucide-react";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+import { Badge } from "./ui/badge";
+
+import JobInfo from "./job-info";
+import DeleteJobButton from "./delete-job-button";
+
 import { JobType } from "@/utils/types";
 
 export default function JobCard({ job }: { job: JobType }) {
-  return <div>JobCard</div>;
+  return (
+    <Card className="bg-muted">
+      <CardHeader>
+        <CardTitle>{job.position}</CardTitle>
+        <CardDescription>{job.company}</CardDescription>
+      </CardHeader>
+      <Separator />
+      <CardContent></CardContent>
+      <CardFooter className="flex gap-4">
+        <Button asChild size="sm">
+          <Link href={`/jobs/${job.id}`}>edit</Link>
+        </Button>
+        <DeleteJobButton />
+      </CardFooter>
+    </Card>
+  );
 }
